d191e5106c3b0138f0f42ee2b7a8ee05b1f4e1de
YuChao Dong <yuchaodong@gmail.com>
2018-07-03 19:34:38 -0400

    completed humidity rendering

--- public/index.js
+++ public/index.js
@@ -47,170 +47,118 @@ class DataTracker {
   }
 }
 
+function testingClickhandler() {
+  const testButton = document.getElementById('testButton2')
 
-function testClickHandlerAndUpdateElem() {
-  const testDisplay = document.getElementById('testDisplay');
-  const testButton = document.getElementById('testButton');
-  //testButton.addEventListener('click', (e) => {
-  //  testDisplay.innerHTML = 123
-  //})
-  testButton.onclick = (e) => {
-    testDisplay.innerHTML = 456
-  }
+  testButton.addEventListener('click', function () {
+    const testText = document.getElementById('testText3')
+    testText.innerHTML = 111;
+  
+    // testText3.innerHTML = 234
+    console.log(111)
+  })  
 }
 
 function testEndpoint() {
-  //const stats = new DataTracker()
-
-  //const API_KEY = '446bc4479be13856e4628226251f3d58'
-  const API_KEY = 'e0b20ff2b6955b76e9e552fb8e239e68'
-  const city = 'london'
-  const country = 'uk'
-  const BASE_URL = 'https://api.openweathermap.org'
-  const forecastEndpoint = '/data/2.5/forecast/daily'
-  const query = `?cnt=16&units=imperial&q=${city},${country}&APPID=${API_KEY}`
-  const endpoint = BASE_URL + forecastEndpoint + query
-
-  axios.get(endpoint)
-    .then(function(response){
-      console.log(response)
-      //for (let dayData of response.data.list) {
-      //  console.log(dayData.humidity)
-      //  stats.insert(dayData.humidity)
-      //}
-      //console.log('STATS')
-      //console.log('min:', stats.showMin())
-      //console.log('max:', stats.showMax())
-      //console.log('mean:', stats.showMean())
-      //console.log('mode:', stats.showMode())
-    });
-}
-function testTracker() {
-  const tracker = new DataTracker()
-
-  const API_KEY = 'e0b20ff2b6955b76e9e552fb8e239e68'
-  const city = 'london'
-  const country = 'uk'
-  const BASE_URL = 'https://api.openweathermap.org'
-  const forecastEndpoint = '/data/2.5/forecast/daily'
-  const query = `?cnt=16&units=imperial&q=${city},${country}&APPID=${API_KEY}`
-  const endpoint = BASE_URL + forecastEndpoint + query
-
-  axios.get(endpoint)
-    .then(function(response){
-      for (let dayData of response.data.list) {
-        console.log(dayData.humidity)
-        tracker.insert(dayData.humidity)
-      }
-      console.log('STATS')
-      console.log('min:', tracker.showMin())
-      console.log('max:', tracker.showMax())
-      console.log('mean:', tracker.showMean())
-      console.log('mode:', tracker.showMode())
-    });
+  const cityName = 'london';
+  const countryCode = 'uk';
+  const BASE_API_URL = 'http://api.openweathermap.org/data/2.5'
+  const FORECAST_ENDPOINT = '/forecast/daily'
+  const queryString = `?cnt=16&APPID=${API_KEY}&q=${cityName},${countryCode}`
+  const endpoint = BASE_API_URL + FORECAST_ENDPOINT + queryString
+  axios.get(endpoint).then(function (data) {
+    console.log(data)
+  })
 }
 
-function testMultipleTrackers() {
-  const humidityTracker = new DataTracker()
-  const mornTemperatureTracker = new DataTracker()
-  const dayTemperatureTracker = new DataTracker()
-  const nightTemperatureTracker = new DataTracker()
-
-  const API_KEY = 'e0b20ff2b6955b76e9e552fb8e239e68'
-  const city = 'london'
-  const country = 'uk'
-  const BASE_URL = 'https://api.openweathermap.org'
-  const forecastEndpoint = '/data/2.5/forecast/daily'
-  const query = `?cnt=16&units=imperial&q=${city},${country}&APPID=${API_KEY}`
-  const endpoint = BASE_URL + forecastEndpoint + query
-
-  axios.get(endpoint)
-    .then(function(response){
-      for (let dayData of response.data.list) {
-        humidityTracker.insert(dayData.humidity)
-        mornTemperatureTracker.insert(dayData.temp.morn)
-        dayTemperatureTracker.insert(dayData.temp.day)
-        nightTemperatureTracker.insert(dayData.temp.night)
-      }
-
-      // print stats
-      console.log('\nHUMIDITY')
-      console.log('min:', humidityTracker.showMin())
-      console.log('max:', humidityTracker.showMax())
-      console.log('mean:', humidityTracker.showMean())
-      console.log('mode:', humidityTracker.showMode())
-      console.log('\nMORN')
-      console.log('min:', mornTemperatureTracker.showMin())
-      console.log('max:', mornTemperatureTracker.showMax())
-      console.log('mean:', mornTemperatureTracker.showMean())
-      console.log('mode:', mornTemperatureTracker.showMode())
-      console.log('\nDAY')
-      console.log('min:', dayTemperatureTracker.showMin())
-      console.log('max:', dayTemperatureTracker.showMax())
-      console.log('mean:', dayTemperatureTracker.showMean())
-      console.log('mode:', dayTemperatureTracker.showMode())
-      console.log('\nNIGHT')
-      console.log('min:', nightTemperatureTracker.showMin())
-      console.log('max:', nightTemperatureTracker.showMax())
-      console.log('mean:', nightTemperatureTracker.showMean())
-      console.log('mode:', nightTemperatureTracker.showMode())
-    });
-}
-
-function testTrackersWithCityCountry(city, country) {
-  const humidityTracker = new DataTracker()
-  const mornTemperatureTracker = new DataTracker()
-  const dayTemperatureTracker = new DataTracker()
-  const nightTemperatureTracker = new DataTracker()
-
-  const API_KEY = 'e0b20ff2b6955b76e9e552fb8e239e68'
-  const BASE_URL = 'https://api.openweathermap.org'
-  const forecastEndpoint = '/data/2.5/forecast/daily'
-  const query = `?cnt=16&units=imperial&q=${city},${country}&APPID=${API_KEY}`
-  const endpoint = BASE_URL + forecastEndpoint + query
-
-  axios.get(endpoint)
-    .then(function(response){
-      for (let dayData of response.data.list) {
-        humidityTracker.insert(dayData.humidity)
-        mornTemperatureTracker.insert(dayData.temp.morn)
-        dayTemperatureTracker.insert(dayData.temp.day)
-        nightTemperatureTracker.insert(dayData.temp.night)
-      }
-
-      // print stats
-      console.log('\nHUMIDITY')
-      console.log('min:', humidityTracker.showMin())
-      console.log('max:', humidityTracker.showMax())
-      console.log('mean:', humidityTracker.showMean())
-      console.log('mode:', humidityTracker.showMode())
-      console.log('\nMORN')
-      console.log('min:', mornTemperatureTracker.showMin())
-      console.log('max:', mornTemperatureTracker.showMax())
-      console.log('mean:', mornTemperatureTracker.showMean())
-      console.log('mode:', mornTemperatureTracker.showMode())
-      console.log('\nDAY')
-      console.log('min:', dayTemperatureTracker.showMin())
-      console.log('max:', dayTemperatureTracker.showMax())
-      console.log('mean:', dayTemperatureTracker.showMean())
-      console.log('mode:', dayTemperatureTracker.showMode())
-      console.log('\nNIGHT')
-      console.log('min:', nightTemperatureTracker.showMin())
-      console.log('max:', nightTemperatureTracker.showMax())
-      console.log('mean:', nightTemperatureTracker.showMean())
-      console.log('mode:', nightTemperatureTracker.showMode())
-    });
+function promisedForecast(cityName, countryCode) {
+  const BASE_API_URL = 'http://api.openweathermap.org/data/2.5'
+  const FORECAST_ENDPOINT = '/forecast/daily'
+  const queryString = `?cnt=16&APPID=${API_KEY}&q=${cityName},${countryCode}`
+  const endpoint = BASE_API_URL + FORECAST_ENDPOINT + queryString
+  return axios.get(endpoint)
 }
 
 
-//testClickHandlerAndUpdateElem()
-//testEndpoint()
-//testTracker()
-//testMultipleTrackers()
-
-// const city = 'seattle'
-// const country = 'us'
-// testTrackersWithCityCountry(city, country)
-
+function testUsingTracker() {
+  let testing = new DataTracker();
+  testing.insert(7)
+  testing.insert(8)
+  testing.insert(9)
+  console.log(testing.showMean());
+}
 
 
+function testTracker () {
+  const humidityTracker = new DataTracker();
+  promisedForecast('london', 'uk').then(function (data) {
+    for (let dayData of data.data.list) {
+      humidityTracker.insert(dayData.humidity)
+    }
+    console.log('humidity mean:', humidityTracker.showMean());
+  })
+}
+
+function promisedHumidityMean (cityName, countryCode) {
+  const humidityTracker = new DataTracker();
+  return promisedForecast(cityName, countryCode).then(function (data) {
+    for (let dayData of data.data.list) {
+      humidityTracker.insert(dayData.humidity)
+    }
+    return humidityTracker.showMean();
+  })
+}
+
+function testButtonToForecast () {
+  const testButton = document.getElementById('testButton2');
+  testButton.addEventListener('click', function () {
+    promisedHumidityMean('london', 'uk')
+      .then(function (data) {
+        let testText = document.getElementById('testText3');
+        testText.innerHTML = data;
+      })
+  })
+}
+
+function testParamsForStats () {
+  const cityName = 'seattle';
+  const countryCode = 'us';
+  promisedHumidityMean(cityName, countryCode).then(function (humidityMean) {
+    console.log(humidityMean)
+  })
+}
+
+function testInput () {
+  const submitButton = document.getElementById('submitButton');
+  const cityInput = document.getElementById('cityInput');
+  submitButton.addEventListener('click', function () {
+    console.log('city name', cityInput.value)    
+  })
+}
+
+function testInputForStats () {
+  const submitButton = document.getElementById('submitButton');
+  const cityInput = document.getElementById('cityInput');
+  const countryInput = document.getElementById('countryInput');
+  const testText = document.getElementById('testText3')
+  submitButton.addEventListener('click', function () {
+    promisedHumidityMean(cityInput.value, countryInput.value).then(function (humidityMean) {
+      testText.innerHTML = humidityMean;
+    })
+  })
+}
+
+
+
+
+
+
+
+// testingClickhandler()
+// testEndpoint()
+// testTracker()
+// testUsingTracker()
+//testButtonToForecast()
+//testParamsForStats()
+// testInput()
+// testInputForStats()
